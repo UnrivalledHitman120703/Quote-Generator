4d7d87adbbc6efea07f55ee3420a8b4af19c8fe0
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-08-16 06:55:06 +0530

    started adding js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello World Testing the project template!!!");
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+  
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    alert(error);
+  }
+}
+
+//On Load
+getQuotes();
